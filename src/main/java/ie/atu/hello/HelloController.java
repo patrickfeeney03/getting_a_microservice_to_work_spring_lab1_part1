4901f2e57179a0e83236ae74ccd057589036adc5
Patrick <g00409592@atu.ie>
2023-09-27 14:48:55 +0100

    First commit

---
+++ src/main/java/ie/atu/hello/HelloController.java
@@ -0,0 +1,12 @@
+package ie.atu.hello;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/bye")
+    public String bye() {
+        return "Bye, World!";
+    }
+}
